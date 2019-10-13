3f8d72de172ab5d010e156c2bcb45475795f7467
probonopd <probonopd@users.noreply.github.com>
2019-10-13 10:43:53 -0400

    qemu-kvm

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,5 +1,5 @@
 # yum -y install lorax anaconda
 # livemedia-creator --ks /eight-livecd/ks/el8-livemedia-minimal.ks --no-virt --resultdir  /eight-livecd/media/el8 --project RHEL-8-beta-live --make-iso --iso-only --iso-name RHEL-8-beta-live-x86_64.iso --releasever 8 --volid RHEL-8-beta-live --title RHEL-beta-live --macboot
 # Command '['losetup', '--find', '--show', '/eight-livecd/media/el8/lmc-disk-na8a0qr3.img']' returned non-zero exit status 1.
-yum -y install lorax
+yum -y install lorax qemu-kvm
 livemedia-creator --ks /eight-livecd/ks/el8-livemedia-minimal.ks --resultdir  /eight-livecd/media/el8 --project RHEL-8-beta-live --make-iso --iso-only --iso-name RHEL-8-beta-live-x86_64.iso --releasever 8 --volid RHEL-8-beta-live --title RHEL-beta-live --macboot

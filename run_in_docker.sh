cc8602ef1123aa1c850ffa1fea38038a80934439
probonopd <probonopd@users.noreply.github.com>
2019-10-13 10:33:56 -0400

    No setenforce

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,3 +1,3 @@
 yum -y install lorax
-setenforce 0
+# setenforce 0 # setenforce: command not found
 livemedia-creator --ks ks/el8-livemedia-minimal.ks --no-virt --resultdir  media/el8 --project RHEL-8-beta-live --make-iso --iso-only --iso-name RHEL-8-beta-live-x86_64.iso --releasever 8 --volid RHEL-8-beta-live --title RHEL-beta-live --macboot

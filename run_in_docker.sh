e8526c6dbfd191fa37194584ca080224b4d5f2bd
probonopd <probonopd@users.noreply.github.com>
2019-10-13 10:29:47 -0400

    Create run_in_docker.sh

---
+++ run_in_docker.sh
@@ -0,0 +1,3 @@
+yum install lorax
+setenforce 0
+livemedia-creator --ks ks/el8-livemedia-minimal.ks --no-virt --resultdir  media/el8 --project RHEL-8-beta-live --make-iso --iso-only --iso-name RHEL-8-beta-live-x86_64.iso --releasever 8 --volid RHEL-8-beta-live --title RHEL-beta-live --macboot
